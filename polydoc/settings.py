8ca86ed1de1f4d052d9b8e92c31f35834cc2be06
Austin Hunt <huntaj@cofc.edu>
2022-02-13 20:02:00 -0500

    add conditional usage of heroku config to settings.py

--- polydoc/settings.py
+++ polydoc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,4 +124,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-django_heroku.settings(locals())
+# Only use heroku configuration if application currently running on heroku
+if 'APPLICATION_ON_HEROKU' in os.environ:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals()) 
