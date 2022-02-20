68561848bf2f677c95d9579b6698fb4a003d53b9
Hunt <huntaj@cofc.edu>
2022-02-20 18:54:46 -0500

    add Page to data model

--- app/models.py
+++ app/models.py
@@ -6,11 +6,9 @@ import datetime
 def user_directory_path(instance, filename):
     return f'documents/{instance.user}/{filename}' 
 
-
 class DocumentContainer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=256, default='Document Container')
-
 
 class Document(models.Model): 
     notes = models.TextField(default='')
@@ -22,3 +20,11 @@ class Document(models.Model):
     file = models.FileField(upload_to=user_directory_path,  blank=True)
     containers = models.ManyToManyField(DocumentContainer,null=True, blank=True)
     
+class Page(models.Model): 
+    image = models.ImageField()
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    index = models.IntegerField(default=0)
+    notes = models.TextField(default='')
+
+# Production DB URL 
+#postgres://bggxldjwcelztm:b2c342e33b32e5ba8ae7d2841c27756348432569246da9395eadd2a078d77251@ec2-35-175-68-90.compute-1.amazonaws.com:5432/d5er2o0pp4glls
