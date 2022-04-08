505c97d777ce0c911fa62c41e7cda59c13452fe1
Austin Hunt <huntaj@g.cofc.edu>
2022-04-07 22:34:41 -0400

    fix staticfile serving in settings.py

--- polydoc/settings.py
+++ polydoc/settings.py
@@ -59,6 +59,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_browser_reload.middleware.BrowserReloadMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -128,10 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = f'{BASE_DIR}/staticfiles'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = f'{BASE_DIR}/media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Testing
 TEST_ROOT = f'{BASE_DIR}/app/tests'
